2edcfcfa1ffeb911a254ec85fe847327e2b5a18d
Altanfiz <altanfizgenity@gmail.com>
2024-06-15 11:44:21 +0700

    feat(MobileNavbar): add icon butotn

--- src/components/navbar/MobileNavbar.svelte
+++ src/components/navbar/MobileNavbar.svelte
@@ -1,3 +1,18 @@
+<script>
+    import {
+  AccountsButton,
+  AddButton,
+  AnalyticsButton,
+  HomeButton,
+  RecordsButton,
+} from '../buttons/icon-button/';
+
+
+</script>
 <nav id="mobile-navbar">
-    mobile navbar
+    <HomeButton/>
+    <RecordsButton/>
+    <AddButton/>
+    <AnalyticsButton/>
+    <AccountsButton/>
 </nav>
