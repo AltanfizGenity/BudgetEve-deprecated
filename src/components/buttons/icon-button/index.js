04add400c79689281ad0c81f6d638400db4af760
Altanfiz <altanfizgenity@gmail.com>
2024-06-15 11:41:02 +0700

    dev(icon-buttons): add module entry point

---
+++ src/components/buttons/icon-button/index.js
@@ -0,0 +1,7 @@
+export { default as AccountsButton } from './AccountsButton.svelte';
+export { default as AddButton } from './AddButton.svelte';
+export { default as AnalyticsButton } from './AnalyticsButton.svelte';
+export { default as HomeButton } from './HomeButton.svelte';
+export { default as RecordsButton } from './RecordsButton.svelte';
+export { default as BackButton } from './BackButton.svelte';
+export { default as RegularTransactionButton } from './RegularTransactionButton.svelte';
