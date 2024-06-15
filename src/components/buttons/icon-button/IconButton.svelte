47f326a2f7b274803de68ae852c71e920f552951
Altanfiz <altanfizgenity@gmail.com>
2024-06-15 11:43:11 +0700

    style(icon-button): add basic style for base icon button components

--- src/components/buttons/icon-button/IconButton.svelte
+++ src/components/buttons/icon-button/IconButton.svelte
@@ -3,5 +3,20 @@
 </script>
 
 <button class={`icon-btn ${className}`}>
-  <slot />
+  <slot class="icon"/>
 </button>
+
+<style>
+    .icon-btn {
+      border: none;
+      /* consider this using variable or dynamic approach */
+      width: 24px;
+      height: 24px;
+      background-color: transparent;
+    }
+    
+    .icon {
+      width: inherit;
+      height: inherit;
+    }
+</style>
