69065149f0256cdb64fdc8ec87fdc908ebc131fc
Altanfiz <altanfizgenity@gmail.com>
2024-06-15 11:48:40 +0700

    style(MobileNavbar): basic style

--- src/components/navbar/MobileNavbar.svelte
+++ src/components/navbar/MobileNavbar.svelte
@@ -16,3 +16,16 @@
     <AnalyticsButton/>
     <AccountsButton/>
 </nav>
+
+<style>
+    #mobile-navbar {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: var(--appSpacing);
+    }
+</style>
