705a126e30b1051bc163b49982d4183d54ba481c
Altanfiz <altanfizgenity@gmail.com>
2024-06-15 08:51:46 +0700

    feat(Home): create homepage skeleton

---
+++ src/pages/Home.svelte
@@ -0,0 +1,6 @@
+<main id="home">
+    <h1>Hello, User!</h1>
+    <div class="accounts-panel">accounts</div>
+    <div class="records-panel">records</div>
+    <div class="analytics-panel">analytics</div>
+</main>
