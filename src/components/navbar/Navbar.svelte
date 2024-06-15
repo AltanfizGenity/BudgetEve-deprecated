bc77549a0f30bf6905d00f6ca8035617823d5607
Altanfiz <altanfizgenity@gmail.com>
2024-06-15 11:08:19 +0700

    feat-style(Navbar): create basic layout and style

--- src/components/navbar/Navbar.svelte
+++ src/components/navbar/Navbar.svelte
@@ -1,3 +1,23 @@
 <nav id="navbar">
-    navbar
+    <div class="icon">
+        menu
+    </div>
+    <h3>title</h3>
+    <div class="icon">
+        avatar
+    </div>
 </nav>
+
+<style>
+    #navbar {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 48px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: var(--appSpacing);
+    }
+</style>
