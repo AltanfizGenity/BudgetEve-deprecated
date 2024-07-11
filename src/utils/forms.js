aad10114b57f38a204eaaeace4e998a70de23501
Altanfiz <altanfizgenity@gmail.com>
2024-07-11 10:36:47 +0700

    feat(utils): add generator id for forms

---
+++ src/utils/forms.js
@@ -0,0 +1,9 @@
+/**
+ * This function is taken from ChatGPT
+ * Consider to use alternative or libraries
+ * @returns string of id
+ */
+export function generateID() {
+  // Example function to generate a unique ID
+  return Math.random().toString(36).substring(2, 15);
+}
