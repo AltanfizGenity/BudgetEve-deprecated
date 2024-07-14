8fbd104b15c6f30de2d4bf54b12c0c4e1f43a763
Altanfiz <altanfizgenity@gmail.com>
2024-07-14 11:35:17 +0700

    feat(accounts): display default budget account list

--- src/pages/Accounts.svelte
+++ src/pages/Accounts.svelte
@@ -1,3 +1,15 @@
+<script>
+  import { budgetAccounts } from "../store/appstore";
+</script>
+
 <section id="accounts">
   <h1>accounts</h1>
+  <div class="account-list">
+    {#each $budgetAccounts as account}
+      <div class="account">
+        <div class="name">{account.name}</div>
+        <div class="amount">{account.amount}</div>
+      </div>
+    {/each}
+  </div>
 </section>
