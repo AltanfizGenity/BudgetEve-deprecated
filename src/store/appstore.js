5296eaa73376280ace7e5f924e6aef2ea1edd5a0
Altanfiz <altanfizgenity@gmail.com>
2024-07-14 11:34:53 +0700

    feat(store): add default budget accounts store

--- src/store/appstore.js
+++ src/store/appstore.js
@@ -1,3 +1,10 @@
 import { writable } from "svelte/store";
+import { getAccountObject } from "../utils/data";
+
+const defaultAccounts = [
+  getAccountObject("cash", "cash", 0, ""),
+  getAccountObject("e-cash", "virtual account", 0, ""),
+];
 
 export let records = writable([]);
+export let budgetAccounts = writable(defaultAccounts);
