2a5eb3e8ed9370c2981b803e48dc2bb037048496
Altanfiz <altanfizgenity@gmail.com>
2024-07-14 11:33:26 +0700

    utils: add account data getter function

---
+++ src/utils/data.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param {string} name
+ * @param {string} type
+ * @param {number} amount
+ * @param {string} note
+ */
+export function getAccountObject(name, type, amount, note) {
+  return { name, type, amount, note };
+}
