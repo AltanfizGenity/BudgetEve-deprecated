eef5acc22c6d64db5c1f25122b4c4c7433078491
Altanfiz <altanfizgenity@gmail.com>
2024-07-16 14:30:23 +0700

    feat(button): add TextButton base component

---
+++ src/components/buttons/text-buttons/TextButton.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let elementClass = "";
+
+  const dispatch = createEventDispatcher();
+
+  function click() {
+    dispatch("click");
+  }
+</script>
+
+<button class={`btn btn-text ${elementClass}`} on:click={click}>
+  <slot />
+</button>
