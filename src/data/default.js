0661875f6ad458c8499de55dc43fdbc0874847b5
Altanfiz <altanfizgenity@gmail.com>
2024-07-21 08:45:04 +0700

    app: add default record category data

---
+++ src/data/default.js
@@ -0,0 +1,98 @@
+export const defaultCategories = {
+  income: [
+    {
+      categoryName: "salary",
+      icon: null,
+    },
+    {
+      categoryName: "freelance",
+      icon: null,
+    },
+    {
+      categoryName: "investment",
+      icon: null,
+    },
+    {
+      categoryName: "rental income",
+      icon: null,
+    },
+    {
+      categoryName: "gifts",
+      icon: null,
+    },
+    {
+      categoryName: "refunds",
+      icon: null,
+    },
+    {
+      categoryName: "bonus",
+      icon: null,
+    },
+    {
+      categoryName: "other",
+      icon: null,
+    },
+  ],
+  expenses: [
+    {
+      categoryName: "gas",
+      icon: null,
+    },
+    {
+      categoryName: "groceries",
+      icon: null,
+    },
+    {
+      categoryName: "rent",
+      icon: null,
+    },
+    {
+      categoryName: "utilities",
+      icon: null,
+    },
+    {
+      categoryName: "entertainment",
+      icon: null,
+    },
+    {
+      categoryName: "dining out",
+      icon: null,
+    },
+    {
+      categoryName: "transportation",
+      icon: null,
+    },
+    {
+      categoryName: "healthcare",
+      icon: null,
+    },
+    {
+      categoryName: "insurance",
+      icon: null,
+    },
+    {
+      categoryName: "education",
+      icon: null,
+    },
+    {
+      categoryName: "clothing",
+      icon: null,
+    },
+    {
+      categoryName: "subscriptions",
+      icon: null,
+    },
+    {
+      categoryName: "savings",
+      icon: null,
+    },
+    {
+      categoryName: "travel",
+      icon: null,
+    },
+    {
+      categoryName: "miscellaneous",
+      icon: null,
+    },
+  ],
+};
