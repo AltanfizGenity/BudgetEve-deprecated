5b552094ba5d79082446109b6a0b09a829794b50
Altanfiz <altanfizgenity@gmail.com>
2024-07-22 11:09:36 +0700

    feat (records): add totalBalance store

--- src/pages/Records.svelte
+++ src/pages/Records.svelte
@@ -1,18 +1,24 @@
 <script>
-  import { records } from "../store/appstore";
+  import { records, totalBalance } from "../store/appstore";
 </script>
 
 <section id="records">
   <h1>Records</h1>
   <div class="type-panel">
-    <div class="income"></div>
-    <div class="expense"></div>
+    <div class="income">
+      <div class="info-type">income</div>
+      <div class="amount">{$totalBalance.income}</div>
+    </div>
+    <div class="expense">
+      <div class="info-type">expense</div>
+      <div class="amount">{$totalBalance.expense}</div>
+    </div>
   </div>
   <div class="record-list">
     {#each $records as record}
       <div class="record">
         <div class="left-info">
-          <div class="type">{record.type}</div>
+          <div class="type">{record.category}</div>
         </div>
         <div class="right-info">
           <div class="amount">{record.amount}</div>
@@ -21,3 +27,6 @@
     {/each}
   </div>
 </section>
+
+<style>
+</style>
--- src/store/appstore.js
+++ src/store/appstore.js
@@ -1,4 +1,4 @@
-import { writable } from "svelte/store";
+import { derived, writable } from "svelte/store";
 import { getAccountObject } from "../utils/data";
 import { defaultCategories } from "../data/default";
 
@@ -10,3 +10,15 @@ const defaultAccounts = [
 export let records = writable([]);
 export let budgetAccounts = writable(defaultAccounts);
 export let categories = writable(defaultCategories);
+
+export let totalBalance = derived(records, ($records) =>
+  $records.reduce(
+    (total, record) => {
+      record.type === "income"
+        ? (total.income += record.amount)
+        : (total.expense += record.amount);
+      return total;
+    },
+    { income: 0, expense: 0 },
+  ),
+);
