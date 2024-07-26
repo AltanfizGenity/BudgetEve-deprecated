31d10cc448ec1a2e271da1c83f72f5eaa1ee1782
Altanfiz <altanfizgenity@gmail.com>
2024-07-26 07:24:32 +0700

    style (accounts): add base style on accounts page

--- src/pages/Accounts.svelte
+++ src/pages/Accounts.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <section id="accounts">
-  <h1>accounts</h1>
+  <h1>Accounts</h1>
   <div class="account-list">
     {#each $budgetAccounts as account}
       <div class="account">
@@ -15,11 +15,39 @@
     {/each}
   </div>
   <div class="actions">
-    <button on:click={() => ($isAccountFormOpen = true)}>new account</button>
-    <button>manage account</button>
+    <button
+      class="btn text-btn primary-btn"
+      on:click={() => ($isAccountFormOpen = true)}>new account</button
+    >
+    <button class="btn text-btn primary-text-btn">manage account</button>
   </div>
 </section>
 
 {#if $isAccountFormOpen}
   <AccountForm />
 {/if}
+
+<style>
+  #accounts {
+    position: relative;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: var(--appSpacing);
+  }
+
+  .account-list {
+    display: flex;
+    flex-direction: column;
+    gap: calc(var(--appSpacing) / 2);
+  }
+
+  .account {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 1rem;
+    background-color: #fff;
+    border-radius: 10px;
+  }
+</style>
