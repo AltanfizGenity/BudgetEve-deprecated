5a45001ea31c705aeb27d45b97dffbd2eec8afb4
Altanfiz <altanfizgenity@gmail.com>
2024-07-30 00:27:27 +0700

    feat: add record component base

---
+++ src/components/core/Record.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let record;
+</script>
+
+<div class="record">
+  <div class="left-info">
+    <div class="type">{record.category}</div>
+  </div>
+  <div class="right-info">
+    <div class="amount">{record.amount}</div>
+  </div>
+</div>
+
+<style>
+  .record {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+    padding: 0.8rem 0;
+  }
+
+  .record:not(:last-child) {
+    border-bottom: 1px solid rgba(200, 200, 200, 0.5);
+  }
+</style>
