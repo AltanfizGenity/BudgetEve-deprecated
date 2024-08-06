dc9c395a32395653f497e5bb6db6c9e2d446ab4f
Altanfiz <altanfizgenity@gmail.com>
2024-08-06 21:34:21 +0700

    lab: use SASS partial and modules

--- src/components/navbar/Navbar.svelte
+++ src/components/navbar/Navbar.svelte
@@ -1,23 +1,9 @@
 <script>
   import Signup from "../buttons/text-buttons/Signup.svelte";
+  import "../../styles/navbar.css";
 </script>
 
 <nav id="navbar">
   <div class="logo">BudgetEve</div>
   <Signup />
 </nav>
-
-<style>
-  #navbar {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: var(--navbarHeight);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: var(--appSpacing);
-    z-index: 100;
-  }
-</style>
